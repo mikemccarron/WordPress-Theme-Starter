82d4f274e58e204f4844e0637fbe8fea5232d596
Mike McCarron <mike@mikemccarron.com>
2013-08-15 17:44:59 -0400

    Updated SCSS components and expanded upon _settings.scss

--- starter-theme/index.php
+++ starter-theme/index.php
@@ -1,13 +1,5 @@
 <?php get_header(); ?>
 
-<ul>
-	<li class="ico ico-Alarm-clock-128x128"></li>
-	<li class="ico ico-Arrival-128x128"></li>
-	<li class="ico ico-Article-128x128"></li>
-	<li class="ico ico-Autofill-128x128"></li>
-	<li class="ico ico-Award-128x128"></li>
-</ul>
-
 <section id="content">
 	<?php get_template_part( 'nav', 'above' ); ?>
 
