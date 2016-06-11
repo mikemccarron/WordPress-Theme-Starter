c3154028c72081efb7db59c72a048abf28bf3f9e
Mike McCarron <mike@mikemccarron.com>
2016-06-10 21:00:47 -0400

    Moved sometimes required files into extras folder

---
+++ starter-theme/_extras/nav-social.php
@@ -0,0 +1,5 @@
+<nav id="nav-social">
+	<a href="#" title="Facebook" class="icon social"><span>Facebook</span></a>
+	<a href="#" title="Twitter" class="icon social"><span>Twitter</span></a>
+	<a href="#" title="Instagram" class="icon social"><span>Instagram</span></a>
+</nav>
