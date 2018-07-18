9aa86305f43d1cfba7467afc2aab416780345c3b
Mike McCarron <mikemccarron@1trickpony.com>
2018-07-18 09:57:12 -0400

    Added Gulp and performance optimizations

--- starter-theme/_production/Gulpfile.js
+++ starter-theme/_production/Gulpfile.js
@@ -1,151 +1,138 @@
 /*
 Required Dependencies
 */
-var gulp 			= require('gulp'),
-	gutil 			= require('gulp-util'),
-	plumber 		= require('gulp-plumber'),
-
-	bower 			= require('main-bower-files'),
-	bowerNormalizer = require('gulp-bower-normalize'),
-
+var gulp 		= require('gulp'),
+	plumber 	= require('gulp-plumber'),
 	babel 		= require('gulp-babel'),
 	babelCore 	= require('babel-core'),
 	babelPoly 	= require('babel-polyfill'),
 	concat 		= require('gulp-concat'),
 	sourcemaps 	= require('gulp-sourcemaps'),
-
 	jshint 		= require('gulp-jshint'),
-	closure 	= require('gulp-closure-compiler'),
-
+	uglify 		= require('gulp-uglify'),
 	sass 		= require('gulp-sass'),
+	csso 		= require('gulp-csso'),
+	svgsprites	= require("gulp-svg-sprites"),
 	prefixer 	= require('gulp-autoprefixer'),
-	// pngmin 		= require('gulp-pngmin'),
-	// pngquant 	= require('imagemin-pngquant'),
-	// imagemin 	= require('gulp-imagemin'),
-	// svgmin 		= require('gulp-svgmin'),
-	// svgstore 	= require('gulp-svgstore'),
-
-	// path 		= require('path'),
-	// rename 		= require('gulp-rename'),
-	// ftp 		= require('vinyl-ftp'),
-
-	notify 		= require("gulp-notify");
+	notify 		= require("gulp-notify"),
+	browserSync = require("browser-sync").create();
 
 /*
 Settings
 */
+var localhost = "http://localhost/";
 var basePaths = {
 	prod: "",
-	dist: "../"
+	dist: "../dist/",
+	distroot: "../dist/"
 }
 
 var settings = {
 	"javascript":{
 		prod: basePaths.prod + 'scripts/',
-		dist: basePaths.dist + 'js/',
+		dist: basePaths.dist + 'assets/scripts/',
 		compiler: ''
 
 	},
 	"css":{
 		prod: basePaths.prod + 'styles/',
-		dist: basePaths.dist + ''
-	},
-	"html":{
-		prod: basePaths.prod,
-		dist: basePaths.dist
+		dist: basePaths.dist + 'assets/styles/'
 	},
 	"images":{
 		prod: basePaths.prod + 'assets/',
-		dist: basePaths.dist + 'img/'
-	},
-	"svgs":{
-		prod: basePaths.prod + '',
-		dist: ''
+		dist: basePaths.dist + 'assets/'
 	}
 }
-
-var deployments = {
-	"live":{
-		host:	 '',
-		user:	 '',
-		password: '',
-	}
-}
-
 
 gulp.task('default', ['watch']);
 
-gulp.task('settings', function(){
-	gutil.log( gutil.colors.bgBlue('Settings') );
-	gutil.log( settings );
-});
-
 gulp.task('watch', function() {
-
-	gutil.log('Starting JS build...');
+	// Init BrowserSync
+	browserSync.init({ proxy: localhost });
 
 	// Watching for JS changes.
-	gulp.watch( settings["javascript"].prod + 'plugins/*.js', ['js-concat']);
-	gulp.watch( settings["javascript"].prod + '**/*.js', ['js']);
+	gulp.watch( settings["javascript"].prod + 'plugins/*.js', ['js-concat', 'js-compress-plugins']);
+
+	gulp.watch(
+		[
+			settings["javascript"].prod + '**/*.js',
+			'!' + settings["javascript"].prod + 'plugins/*.js'
+		],
+		['js']
+	);
+
+	// Render SVG Sprites
+	gulp.watch( settings["css"].prod +'**/*.scss', ['sass', 'css-prefix']);
 
 	// Rendering CSS
-	gulp.watch( settings["css"].prod +'**/*.scss', ['sass', 'prefix-css']);
+	gulp.watch( settings["images"].prod +'svgs/*.svg', ['build-svg-sprite']);
+
+	// Reload page when .html files change
+	gulp.watch(basePaths.distroot + "templates/**/*.html").on('change', browserSync.reload);
+});
+
+/* ------------------------------
+SVG Spritesheets
+------------------------------*/
+gulp.task('build-svg-sprite', function () {
+	return gulp.src( settings["images"].prod +'svgs/*.svg' )
+		.pipe( svgsprites({
+			mode: "symbols"
+		}))
+		.pipe( gulp.dest( settings["images"].dist + 'vectorsprites/' ));
 });
 
 /* ------------------------------
 JS
 ------------------------------*/
 gulp.task('js', function() {
-	gutil.log( gutil.colors.bold.bgBlue('Compiling Javascript')) ;
-	gutil.log( gutil.colors.cyan('\t[' + settings["javascript"].prod + '] => [' + settings["javascript"].dist + ']') );
-	gutil.log( gutil.colors.cyan('\tRunning through JSHint...') );
-	gutil.log( gutil.colors.cyan('\tRunning through Babel...') );
-	gutil.log('\n');
+	console.log('Running gulp.task: js');
 
-	return gulp.src( settings["javascript"].prod + '*.js')
+	return gulp.src([
+			'!' + settings["javascript"].prod + 'plugins/*.js',
+			settings["javascript"].prod + 'modules/*.js',
+			settings["javascript"].prod + '*.js'
+
+		])
 		.pipe( plumber({
 			errorHandler: notify.onError("Error: <%= error.message %>")
 		}) )
-		.pipe(sourcemaps.init())
+		.pipe( sourcemaps.init() )
 		.pipe( jshint() )
-		.pipe( babel({
-            presets: ['env']
-		}) )
 		.pipe( concat('app.js') )
+		.pipe( babel({ presets: ['env'] }))
 		.pipe( sourcemaps.write('.') )
+		.pipe( plumber.stop() )
+		.pipe( gulp.dest( settings["javascript"].dist ))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('js-compress', function(){
+	return gulp.src( settings["javascript"].dist +'**/*.js')
+		.pipe( plumber({ errorHandler: notify.onError("Error: <%= error.message %>")} ))
 		.pipe( plumber.stop() )
 		.pipe( gulp.dest( settings["javascript"].dist ));
 });
 
-gulp.task('compress-js', function(){
-	gutil.log( gutil.colors.blue('Compressing JS ['+ settings["javascript"].dist +'app.js] with Closure Compiler...') );
-	gutil.log('\n');
-
-	return gulp.src( settings["javascript"].dist +'**/*.js')
+gulp.task('js-compress-plugins', function(){
+	return gulp.src( settings["javascript"].dist +'plugins/plugins.js')
 		.pipe( plumber({ errorHandler: notify.onError("Error: <%= error.message %>")} ))
-		.pipe(closureCompiler({
-			compilerPath:  settings["javascript"].compiler,
-			fileName: 'scripts.min.js'
-		}))
 		.pipe( plumber.stop() )
-		.pipe( gulp.dest( settings["javascript"].dist ));
+		.pipe( uglify() )
+		.pipe( gulp.dest( settings["javascript"].dist + 'plugins' ));
 });
 
 gulp.task('js-concat', function() {
-	gutil.log( gutil.colors.bold.bgBlue('\nNew Plugin Found:') );
-	gutil.log( gutil.colors.cyan('\tCombining it to [' + settings["javascript"].dist + 'plugins]' + ' file...') );
-	gutil.log('\n');
-
 	return gulp.src( settings["javascript"].prod+'plugins/*.js' )
 		.pipe( concat('plugins.js'))
-		.pipe( gulp.dest( settings["javascript"].dist + 'plugins' ));
+		.pipe( gulp.dest( settings["javascript"].dist + 'plugins' ))
+		.pipe(browserSync.stream());
 });
 
 /* ------------------------------
 CSS
 ------------------------------*/
 gulp.task('sass', function() {
-	gutil.log( gutil.colors.bold.bgGreen('Compiling SCSS to CSS...') );
 	return gulp.src( settings["css"].prod + '**/*.scss')
 		.pipe( plumber({ errorHandler: notify.onError("Error: <%= error.message %>")} ))
 		.pipe( sass({
@@ -158,18 +145,12 @@ gulp.task('sass', function() {
 		.pipe( gulp.dest( settings["css"].dist ));
 });
 
-gulp.task('prefix-css', ['sass'], function () {
-	gutil.log( gutil.colors.bgGreen('\nAppending browsers prefixes [\'last 2 versions\', \'> 5%\', \'Explorer > 10\'] to CSS...\n') );
+gulp.task('css-prefix', ['sass'], function () {
 	return gulp.src( settings["css"].dist + '*.css')
 		.pipe( plumber({ errorHandler: notify.onError("Error: <%= error.message %>")} ))
 		.pipe( prefixer({ browsers: ['last 2 versions', '> 5%', 'Explorer > 10'] }))
+		.pipe( csso() )
 		.pipe( plumber.stop() )
-		.pipe( gulp.dest( settings["css"].dist ) );
-});
-
-/* ------------------------------
-Deployments
-------------------------------*/
-gulp.task('deploy', function(){
-
+		.pipe( gulp.dest( settings["css"].dist ) )
+		.pipe(browserSync.stream());
 });
--- starter-theme/footer.php
+++ starter-theme/footer.php
@@ -1,3 +1,15 @@
+	<!-- http://www.stevesouders.com/blog/2009/04/27/loading-scripts-without-blocking/ -->
+	<script type="text/javascript">
+
+		function deferScriptLoader(src) {
+			window._sf_endpt=(new Date()).getTime();
+			var script = document.createElement("script");
+			document.getElementsByTagName("body")[0].appendChild(script);
+			script.src = src;
+		}
+		deferScriptLoader("<?php bloginfo('siteurl'); ?>assets/scripts/plugins/plugins.js");
+		deferScriptLoader("<?php bloginfo('siteurl'); ?>assets/scripts/app.js");
+	</script>
 
 	<?php wp_footer(); ?>
 </body>
